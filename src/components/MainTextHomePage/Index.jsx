e4adb2fb4f2adfe2b456bfa3b0e4d623f94363f9
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-09-22 14:58:04 -0300

    setting responsiveness

--- src/components/MainTextHomePage/Index.jsx
+++ src/components/MainTextHomePage/Index.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import style from "./mainText.module.scss"
+import React from "react";
+import style from "./mainText.module.scss";
 
 const MainTextHomePage = () => {
   return (
-    <div className={style.mainTextContainer}>
+    <>
+      <div className={style.mainTextContainer}>
         <h1>HOME & DECOR</h1>
         <hr />
-        <p>VÁRIOS ITENS DE DECORAÇÃO QUE VÃO DEIXAR SEU AMBIENTE MAIS ELEGANTE</p>
-    </div>
-  )
-}
+        <p>
+          VÁRIOS ITENS DE DECORAÇÃO QUE VÃO DEIXAR SEU AMBIENTE MAIS ELEGANTE
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default MainTextHomePage
+export default MainTextHomePage;
