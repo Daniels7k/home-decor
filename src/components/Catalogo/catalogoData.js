8cdcd97ce75020f92f332093fe9909880056ba32
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-09-22 10:56:12 -0300

    Catalogo component

---
+++ src/components/Catalogo/catalogoData.js
@@ -0,0 +1,47 @@
+export const catalogoData = [
+    {
+        img: "products/urso.png",
+        name: "URSOS DE CERÂMICA",
+        price: "199,99",
+    },
+    {
+        img: "products/globo.png",
+        name: "PEÇAS DE OURO COM GLOBOS AZUIS",
+        price: "1.999,99",
+    },
+    {
+        img: "products/jarras.png",
+        name: "JARRAS DECORATIVAS",
+        price: "299,00",
+    },
+    {
+        img: "products/centro.png",
+        name: "CENTRO DE MESA COMPLETO",
+        price: "399,99",
+    },
+    {
+        img: "products/cama.png",
+        name: "CAMA DE CASAL PRINCESS",
+        price: "29.999,00",
+    },
+    {
+        img: "products/vasos.png",
+        name: "VASOS DECORATIVOS",
+        price: "159,99",
+    },
+    {
+        img: "products/ornamental.png",
+        name: "PEÇA ORNAMENTAL",
+        price: "299,99",
+    },
+    {
+        img: "products/vasoMae.png",
+        name: "VASO MÃE PRESENTEAVEL",
+        price: "99,00",
+    },
+    {
+        img: "products/espelho.png",
+        name: "ESPELHOS PRICESS",
+        price: "199,99",
+    },
+]
---
+++ src/components/Catalogo/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import style from "./catalogo.module.scss";
+
+import { catalogoData } from "./catalogoData";
+
+const Catalogo = () => {
+  return (
+    <div className={style.catalogoContainer}>
+      <div className={style.header}>
+        <h3>CATALÓGO</h3>
+      </div>
+
+      <div className={style.productsRender}>
+        {catalogoData.map((item, i) => (
+          <div className={style.productBox} key={i} >
+            <div className={style.productImage}>
+              <img src={item.img} alt={item.nome} />
+            </div>
+            <div className={style.productBottom}>
+              <p className={style.productTitle}>{item.name}</p>
+              <p className={style.productPrice}>R$ {item.price} (Cada)</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Catalogo;
