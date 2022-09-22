7bad9fd6879579120cb2b6fb9fa149000acdf6af
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-09-22 09:42:27 -0300

    MainTextHomePage component

---
+++ src/components/MainTextHomePage/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from "./mainText.module.scss"
+
+const MainTextHomePage = () => {
+  return (
+    <div className={style.mainTextContainer}>
+        <h1>HOME & DECOR</h1>
+        <hr />
+        <p>VÁRIOS ITENS DE DECORAÇÃO QUE VÃO DEIXAR SEU AMBIENTE MAIS ELEGANTE</p>
+    </div>
+  )
+}
+
+export default MainTextHomePage
