67dc0c9161abb87d58939a20746dae13739061a2
Cleiton Daniel <87211930+Daniels7k@users.noreply.github.com>
2022-09-22 15:19:10 -0300

    setting components on app page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Background from "./components/Background";
+import MainTextHomePage from "./components/MainTextHomePage/Index";
+import Catalogo from "./components/Catalogo";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Background>
+    <Navbar />
+    <MainTextHomePage />
+    </Background>
+
+    <Catalogo />
+
+    <Footer />
+  </>
   );
 }
 
